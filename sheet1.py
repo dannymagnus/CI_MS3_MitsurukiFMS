7e21ef87bd8da05ba1ee4f295f3306dc1df74582
dannymagnus <mrdanrichards@gmail.com>
2021-10-24 22:04:16 +0100

    remove test print statements

--- sheet1.py
+++ sheet1.py
@@ -13,7 +13,6 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('vehiclebookings')
 catalogue = SHEET.worksheet('catalogue')
-c_data = catalogue.get_all_values()
 appraisals = SHEET.worksheet('appraisals')
 
 
